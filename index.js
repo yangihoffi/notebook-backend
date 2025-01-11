a0fd2ce0a3cf51f7d399119f2bcf0b18c6ae8faa
Huu Vuong Phan <166980411+yangihoffi@users.noreply.github.com>
2025-01-11 17:52:16 +0700

    Connect to mongo db and get all notes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { v4: uuidv4 } = require("uuid");
+const mongoose = require("mongoose");
 
 let notes = [
   {
@@ -22,6 +24,25 @@ let notes = [
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const url = process.env.MONGODB_URI;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+noteSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const Note = mongoose.model("Note", noteSchema);
 
 app.use(express.json());
 app.use(cors());
@@ -32,7 +53,9 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/notes", (req, res) => {
-  res.json(notes);
+  Note.find({}).then((notes) => {
+    res.json(notes);
+  });
 });
 
 app.get("/api/notes/:id", (req, res) => {
