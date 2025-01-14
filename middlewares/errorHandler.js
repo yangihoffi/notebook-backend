fccd5fd5443786aa318a3e1beda017a9d7ec8581
Huu Vuong Phan <166980411+yangihoffi@users.noreply.github.com>
2025-01-14 21:07:14 +0700

    Reorganize middlewares

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,13 @@
+const errorHandler = (error, req, res, next) => {
+  console.log(error);
+
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return res.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+module.exports = errorHandler;
---
+++ middlewares/requestLogger.js
@@ -0,0 +1,12 @@
+const logger = require("../utils/logger");
+
+const requestLogger = (req, res, next) => {
+  logger.info("Method:", req.method);
+  logger.info("Path:  ", req.path);
+  logger.info("Body:  ", req.body);
+  logger.info("---");
+
+  next();
+};
+
+module.exports = requestLogger;
