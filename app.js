791f8e1fb66b25567fde4a54c2654303b05d6195
Huu Vuong Phan <166980411+yangihoffi@users.noreply.github.com>
2025-01-14 21:07:51 +0700

    Refactor app

--- app.js
+++ app.js
@@ -2,24 +2,21 @@ const express = require("express");
 const cors = require("cors");
 const notesRouter = require("./controllers/notes.controller");
 
+// middlewares
+const errorHandler = require("./middlewares/errorHandler");
+const requestLogger = require("./middlewares/requestLogger");
+const unknownEndpoint = require("./middlewares/unknownEndpoint");
+
 const app = express();
-
-const errorHandler = (error, req, res, next) => {
-  console.log(error);
-
-  if (error.name === "CastError") {
-    return res.status(400).send({ error: "malformatted id" });
-  }
-
-  next(error);
-};
 
 app.use(cors());
 app.use(express.static("dist"));
 app.use(express.json());
+app.use(requestLogger);
 
 app.use("/api/notes", notesRouter);
 
+app.use(unknownEndpoint);
 app.use(errorHandler);
 
 module.exports = app;
